d67128d725a62cf44fcf37226cdf4754294cb03b
Bartek Dmitruk <bartek.dmitruk@gmail.com>
2023-03-03 20:06:02 +0100

    Adjustments.

--- main.py
+++ main.py
@@ -25,19 +25,18 @@ df = pd.read_csv(f'market_data_{interval}.csv')
 df = df.sort_values(by="Open time")
 df.reset_index(drop=True, inplace=True)
 
-price_delta = 1.01
+price_delta = 1.012
 
 np.seterr(divide='ignore', invalid='ignore')
 df = add_all_ta_features(df, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
 
-df['exp'] = np.where((df['Close'] * price_delta <= df['High'].shift(-1))
-                     | (df['Close'] * price_delta <= df['High'].shift(-2))
-                     | (df['Close'] * price_delta <= df['High'].shift(-3))
-                     | (df['Close'] * price_delta <= df['High'].shift(-4))
-                     | (df['Close'] * price_delta <= df['High'].shift(-5))
-                     | (df['Close'] * price_delta <= df['High'].shift(-6))
-                     | (df['Close'] * price_delta <= df['High'].shift(-7)),
-                     1, 0)
+num_intervals = 12
+conditions = [
+    (df['Close'] * price_delta <= df['High'].shift(-i))
+    for i in range(1, num_intervals + 1)
+]
+conditions_array = np.array(conditions)
+df['exp'] = np.where(conditions_array.any(axis=0), 1, 0)
 
 print('\nCounts of expected values :')
 print(df['exp'].value_counts())
@@ -52,6 +51,8 @@ df.dropna(inplace=True)
 number_of_prediction_klines = 1000
 df_train = df.iloc[:df.shape[0] - number_of_prediction_klines, :]
 df_dev = df.iloc[df.shape[0] - number_of_prediction_klines:, :]
+
+df_dev = df_dev.drop(df_dev.index[-num_intervals:])
 
 df_train.reset_index(drop=True, inplace=True)
 df_dev.reset_index(drop=True, inplace=True)
@@ -82,7 +83,7 @@ x_train_scaled = scaler.transform(x_train)
 x_test_scaled = scaler.transform(x_test)
 x_dev_scaled = scaler.transform(x_dev)
 
-pca = PCA(n_components=16)
+pca = PCA(n_components=24)
 pca.fit(x_train_scaled)
 joblib.dump(pca, dir_name + '/pca.joblib')
 
@@ -104,7 +105,7 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 
 model.compile(loss='binary_crossentropy', optimizer="Adam", metrics=[tf.keras.metrics.Precision()])
 
-model.fit(x_train_PCA, y_train, batch_size=1, epochs=100, validation_split=0.2, validation_data=None, shuffle=True,
+model.fit(x_train_PCA, y_train, batch_size=1, epochs=2, validation_split=0.2, validation_data=None, shuffle=True,
           callbacks=[early_stopping])
 print("")
 
@@ -138,31 +139,28 @@ m = tf.keras.metrics.Precision()
 m.update_state(predictions['target'], predictions['predictions'])
 
 precision = round(m.result().numpy(), 1)
-num_decisions = predictions['predictions'].sum()
 num_good_decision = len(predictions[(predictions['predictions'] == 1) & (predictions['target'] == 1)])
-num_bad_decision = len(predictions[(predictions['predictions'] == 1) & (predictions['target'] == 0)])
-num_opportunities = len(predictions['target'] == 1)
+num_opportunities = len(predictions[(predictions['target'] == 1)])
+pct_good_decision = num_good_decision / num_opportunities
 
-print("\nPrecision : " + str(precision))
-print("Number of decision taken : " + str(num_decisions))
-print("Number of good decision taken : " + str(num_good_decision))
-print("Number of bad decision taken : " + str(num_bad_decision))
-print("Number of opportunities : " + str(num_opportunities))
+print(f"\nVal precision: {str(round(precision * 100, 1))}%")
+print(f"Test Precision: {str(round(evaluation[1] * 100, 1))}%")
+print(f"Percentage of taken opportunities: {str(round(pct_good_decision * 100, 1))}%")
 
 with open(new_dir_name + '/results.txt', 'w') as f:
     f.write(f"Interval: {interval}\n")
-    f.write(f"PCA n_components: {pca.n_components}\n")
+    f.write(f"Price delta: {price_delta}\n")
+    f.write(f"Num intervals: {num_intervals}\n")
+    f.write(f"PCA n_components: {pca.n_components}\n\n")
+    f.write(f"Val precision: {str(round(precision * 100, 1))}%\n")
+    f.write(f"Test Precision: {str(round(evaluation[1] * 100, 1))}%\n")
+    f.write(f"Percentage of taken opportunities: {str(round(pct_good_decision * 100, 1))}%\n\n")
     model.summary(print_fn=lambda x: f.write(x + '\n'))
-    f.write(f"Precision: {precision}\n")
-    f.write(f"Precision: {str(round(evaluation[1] * 100, 1))}\n")
-    f.write(f"Number of decisions taken: {num_decisions}\n")
-    f.write(f"Number of good decisions taken: {num_good_decision}\n")
-    f.write(f"Number of bad decisions taken: {num_bad_decision}\n")
-    f.write(f"Number of opportunities: {num_opportunities}\n")
 
 f.close()
 
-final_dir_name = 'saved_models/model_P-' + str(round(precision * 100, 1)) + '%_' + 'T-' + str(round(evaluation[1] * 100, 1)) + '%'
+final_dir_name = 'saved_models/model_P-' + str(round(precision * 100, 1)) + '%_' + 'T-' + str(
+    round(evaluation[1] * 100, 1)) + '%'
 
 try:
     os.rename(new_dir_name, final_dir_name)
