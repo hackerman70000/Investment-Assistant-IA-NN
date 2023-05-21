d0bfcd02b9a22331b84554448e62972c15565d22
bdmitruk <01178272@pw.edu.pl>
2023-05-21 19:45:25 +0200

    Refactor code.

--- market_data_scraper.py
+++ market_data_scraper.py
@@ -1,44 +1,71 @@
 import datetime
+import os
 import time
+
 import pandas as pd
 import pytz
 import requests
 from requests.exceptions import RequestException
 
-symbol = "BTCUSDT"
-interval = "1h"
-limit = 1000
 
-timezone = pytz.timezone('Europe/Warsaw')
+class market_data_scraper:
 
-end_time = int(time.time() * 1000)
-start_time = int(datetime.datetime(2021, 2, 28, tzinfo=pytz.utc).astimezone(timezone).timestamp() * 1000)
+    def __init__(self, symbol, interval, limit, directory, filename):
+        self.symbol = symbol
+        self.interval = interval
+        self.limit = limit
+        self.timezone = pytz.timezone('Europe/Warsaw')
+        self.directory = directory
+        self.filename = filename
 
-url = f"https://data.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={1000}"
-data = []
+    def download_market_data(self):
+        end_time = int(time.time() * 1000)
+        start_time = int(datetime.datetime(2021, 2, 28, tzinfo=pytz.utc).astimezone(self.timezone).timestamp() * 1000)
+        url = f"https://data.binance.com/api/v3/klines?symbol={self.symbol}&interval={self.interval}&limit={self.limit}"
+        data = []
 
-while start_time < end_time:
-    try:
-        response = requests.get(url + f"&startTime={start_time}&endTime={end_time}")
-        response.raise_for_status()
-        klines = response.json()
-        data += klines
-        start_time = klines[-1][6] + 1
-    except RequestException as e:
-        print("Error occurred:", e)
-        url = f"https://data.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}"
-        time.sleep(60)
+        while start_time < end_time:
+            try:
+                response = requests.get(url + f"&startTime={start_time}&endTime={end_time}")
+                response.raise_for_status()
+                klines = response.json()
+                data += klines
+                start_time = klines[-1][6] + 1
+            except RequestException as e:
+                print("Error occurred:", e)
+                url = f"https://data.binance.com/api/v3/klines?symbol={self.symbol}&interval={self.interval}&limit={self.limit}"
+                time.sleep(60)
 
-df = pd.DataFrame(data,
-                  columns=["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volume",
-                           "Number of trades", "Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"])
+        df = pd.DataFrame(data,
+                          columns=["Open time", "Open", "High", "Low", "Close", "Volume", "Close time",
+                                   "Quote asset volume", "Number of trades", "Taker buy base asset volume",
+                                   "Taker buy quote asset volume", "Ignore"])
 
-df["Open time"] = pd.to_datetime(df["Open time"], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert(timezone)
-df["Close time"] = pd.to_datetime(df["Close time"], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert(timezone)
+        df["Open time"] = pd.to_datetime(df["Open time"], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert(
+            self.timezone)
+        df["Close time"] = pd.to_datetime(df["Close time"], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert(
+            self.timezone)
 
-try:
-    df.to_csv(f'market_data_{interval}.csv', index=False)
-    print(f"Data has been successfully saved to market_data_{interval}.csv")
-except Exception as e:
-    print(f"Error saving data to market_data_{interval}.csv: {e}")
+        return df
 
+    def save_market_data_to_csv(self, df):
+        path = f"{self.directory}/{self.directory}_{self.interval}.csv"
+
+        try:
+            if not os.path.exists(self.directory):
+                os.makedirs(self.directory)
+            df.to_csv(path, index=False)
+            print(f"Data has been successfully saved to {path}")
+        except Exception as e:
+            print(f"Error saving data to {path}: {e}")
+
+
+if __name__ == '__main__':
+    symbol = "BTCUSDT"
+    interval = "1h"
+    limit = 1000
+    directory = "training_data"
+    filename = f"market_data_{interval}.csv"
+
+    data_frame = market_data_scraper(symbol, interval, limit, directory, filename).download_market_data()
+    market_data_scraper(symbol, interval, limit, directory, filename).save_market_data_to_csv(data_frame)
--- predictions.py
+++ predictions.py
@@ -1,6 +1,5 @@
 import datetime
 import os
-import time
 
 import numpy as np
 import pandas as pd
@@ -8,99 +7,106 @@ import pytz
 import requests
 import tensorflow as tf
 from joblib import load
-from requests.exceptions import RequestException
 from ta import add_all_ta_features
 
-symbol = "BTCUSDT"
-interval = "1h"
-price_delta = 1.015
-num_intervals = 12
-timezone = pytz.timezone('Europe/Warsaw')
+from market_data_scraper import market_data_scraper
 
-url = f"https://data.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={1000}"
-data = []
 
-end_time = int(time.time() * 1000)
-start_time = int(datetime.datetime(2021, 2, 28).timestamp() * 1000)
+class PredictionModel:
+    def __init__(self, symbol, interval, price_delta, num_intervals, timezone, limit, directory, filename):
+        self.symbol = symbol
+        self.interval = interval
+        self.price_delta = price_delta
+        self.num_intervals = num_intervals
+        self.timezone = timezone
+        self.limit = limit
+        self.directory = directory
+        self.filename = filename
+        self.path = f"{directory}/{filename}_{interval}.csv"
+        self.scraper = market_data_scraper(symbol, interval, limit, directory, filename)
+        self.entry_price = None
+        self.last_datetime = None
+        self.df = None
+        self.total_delta = None
 
-while start_time < end_time:
-    try:
-        response = requests.get(url + f"&startTime={start_time}&endTime={end_time}")
-        response.raise_for_status()
-        klines = response.json()
-        data += klines
-        start_time = klines[-1][6] + 1
-    except RequestException as e:
-        print("Error occurred:", e)
-        time.sleep(60)
+    def run_prediction(self):
+        self.download_market_data()
+        self.preprocess_data()
+        self.load_models()
+        self.predict()
+        self.print_results()
+        self.get_target_prices()
 
-print("\nData has been successfully downloaded\n")
+    def download_market_data(self):
+        self.df = self.scraper.download_market_data()
+        self.df = self.df.drop(self.df.index[-1])
+        self.entry_price = pd.to_numeric(self.df.iloc[-1]['Close'])
+        self.scraper.save_market_data_to_csv(self.df)
+        self.last_datetime = pd.to_datetime(self.df.iloc[-1]['Close time']) + datetime.timedelta(seconds=1)
+        self.last_datetime = self.last_datetime.replace(microsecond=0, tzinfo=None)
 
-df = pd.DataFrame(data,
-                  columns=["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volume",
-                           "Number of trades", "Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"])
+    def preprocess_data(self):
+        interval_units = {"m": "minutes", "h": "hours", "d": "days", "w": "weeks", "M": "months", "y": "years"}
+        interval_unit = self.interval[-1]
+        interval_num = int(self.interval[:-1])
+        self.total_delta = self.num_intervals * datetime.timedelta(**{interval_units[interval_unit]: interval_num})
 
-df["Open time"] = pd.to_datetime(df["Open time"], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert(timezone)
-df["Close time"] = pd.to_datetime(df["Close time"], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert(timezone)
+        self.df = pd.read_csv(self.path)
+        self.df = self.df.sort_values(by="Open time")
+        self.df.reset_index(drop=True, inplace=True)
+        np.seterr(divide='ignore', invalid='ignore')
+        self.df = add_all_ta_features(self.df, open="Open", high="High", low="Low", close="Close", volume="Volume",
+                                      fillna=True)
+        self.df.drop(['High', 'Open', 'Close', 'Low', 'Volume', 'Quote asset volume', 'Number of trades',
+                      'Taker buy base asset volume',
+                      'Taker buy quote asset volume', 'Ignore', 'Open time', 'Close time'], inplace=True, axis=1)
+        self.df.dropna(inplace=True)
 
-df = df.drop(df.index[-1])
-entry_price = pd.to_numeric(df.iloc[-1]['Close'])
+        self.x_pred = self.df.iloc[self.df.shape[0] - 1:, :]
 
-df.to_csv('prediction_data.csv', index=False)
+    def load_models(self):
+        pca = load('IA-NN-models/pca.joblib')
+        scaler = load('IA-NN-models/scaler.joblib')
+        self.x_pred_scaled = scaler.transform(self.x_pred)
+        self.x_pred_PCA = pca.transform(self.x_pred_scaled)
 
-last_datetime = pd.to_datetime(df.iloc[-1]['Close time']) + datetime.timedelta(seconds=1)
-last_datetime = last_datetime.replace(microsecond=0, tzinfo=None)
+    def predict(self):
+        if os.path.exists('IA-NN-models'):
+            for file_name in os.listdir('IA-NN-models'):
+                if file_name.endswith('.h5'):
+                    model_path = os.path.join('IA-NN-models', file_name)
+                    model = tf.keras.models.load_model(model_path)
+                    y_pred = model.predict(self.x_pred_PCA)
+                    print(f"\nPrediction from {file_name}: {y_pred[0].item():.4f}")
+        else:
+            print("\nError: Model directory not found")
 
-interval_units = {"m": "minutes", "h": "hours", "d": "days", "w": "weeks", "M": "months", "y": "years"}
-interval_unit = interval[-1]
-interval_num = int(interval[:-1])
-total_delta = num_intervals * datetime.timedelta(**{interval_units[interval_unit]: interval_num})
+    def print_results(self):
+        print(f"\nPrediction is valid at: {self.last_datetime:%Y-%m-%d %H:%M:%S}")
+        print(f"Sell until:             {(self.last_datetime + self.total_delta):%Y-%m-%d %H:%M:%S}")
 
-df = pd.read_csv('prediction_data.csv')
+    def get_target_prices(self):
+        url_price_USDT = "https://data.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+        response = requests.get(url_price_USDT).json()
+        price_USDT = float(response["price"])
 
-df = df.sort_values(by="Open time")
-df.reset_index(drop=True, inplace=True)
+        url_price_BUSD = "https://data.binance.com/api/v3/ticker/price?symbol=BTCBUSD"
+        response = requests.get(url_price_BUSD).json()
+        price_BUSD = float(response["price"])
 
-np.seterr(divide='ignore', invalid='ignore')
-df = add_all_ta_features(df, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
+        print(f"\nTarget price USDT: {round(price_USDT * self.price_delta, 2)}")
+        print(f"Target price BUSD: {round(price_BUSD * self.price_delta, 2)}")
 
-df.drop(
-    ['High', 'Open', 'Close', 'Low', 'Volume', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume',
-     'Taker buy quote asset volume', 'Ignore', 'Open time', 'Close time'],
-    inplace=True, axis=1)
 
-df.dropna(inplace=True)
+if __name__ == "__main__":
+    symbol = "BTCUSDT"
+    interval = "1h"
+    price_delta = 1.015
+    num_intervals = 12
+    timezone = pytz.timezone('Europe/Warsaw')
+    limit = 1000
+    directory = "prediction_data"
+    filename = "prediction_data"
 
-x_pred = df.iloc[df.shape[0] - 1:, :]
-
-pca = load('IA-NN-models/pca.joblib')
-scaler = load('IA-NN-models/scaler.joblib')
-
-x_pred_scaled = scaler.transform(x_pred)
-x_pred_PCA = pca.transform(x_pred_scaled)
-
-if os.path.exists('IA-NN-models'):
-    for file_name in os.listdir('IA-NN-models'):
-        if file_name.endswith('.h5'):
-            model_path = os.path.join('IA-NN-models', file_name)
-            model = tf.keras.models.load_model(model_path)
-            y_pred = model.predict(x_pred_PCA)
-            print(f"Prediction from {file_name}: {y_pred[0].item():.4f}")
-else:
-    print("\nError: Model directory not found")
-
-print(
-    f"\nPrediction is valid at: {last_datetime:%Y-%m-%d %H:%M:%S}")
-print(
-    f"Sell within time frame: {last_datetime:%Y-%m-%d %H:%M:%S} - {(last_datetime + total_delta):%Y-%m-%d %H:%M:%S}")
-
-url_price_USDT = "https://data.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-response = requests.get(url_price_USDT).json()
-price_USDT = float(response["price"])
-
-url_price_BUSD = "https://data.binance.com/api/v3/ticker/price?symbol=BTCBUSD"
-response = requests.get(url_price_BUSD).json()
-price_BUSD = float(response["price"])
-
-print(f"\nTarget price USDT: {round(price_USDT * price_delta, 2)}")
-print(f"Target price BUSD: {round(price_BUSD * price_delta, 2)}")
+    model = PredictionModel(symbol, interval, price_delta, num_intervals, timezone, limit, directory, filename)
+    model.run_prediction()
